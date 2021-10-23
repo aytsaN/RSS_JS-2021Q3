d57fcf83c996dab3117742a58009aaf0f0a040ed
Ana Shamal <aytsan.shamal@gmail.com>
2021-10-23 18:47:05 +0300

    feat: implement date time

---
+++ momentum/js/time-date.js
@@ -0,0 +1,26 @@
+// ----------------date
+const date = document.querySelector('.date');
+
+function showDate() {
+  const dateObj = new Date();
+  const options = {month: 'long', weekday: 'long', day: 'numeric'};
+  const currentDate = dateObj.toLocaleDateString(undefined, options);
+  date.textContent = `${currentDate}`;
+}
+
+// ----------------time
+const time = document.querySelector('.time');
+
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString('en-GB');
+  time.textContent = currentTime;
+}
+
+function updateDateTime() {
+  showDate();
+  showTime()
+  setTimeout(updateDateTime, 1000);
+}
+
+updateDateTime();
