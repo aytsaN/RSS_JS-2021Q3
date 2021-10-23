c35b43c1b251eb724d5315010d3b1d9bb4e1a52f
Ana Shamal <aytsan.shamal@gmail.com>
2021-10-23 20:41:56 +0300

    feat: implement bg slider

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -22,7 +22,7 @@ const greetingContainer = document.querySelector('.greeting-container');
 const greetingText = greetingContainer.querySelector('.greeting');
 
 function getTimeOfDay() {
-  const timeOfDayArr = ['Morning', 'Afternoon ', 'Evening', 'Night'];
+  const timeOfDayArr = ['morning', 'afternoon ', 'evening', 'night'];
   const date = new Date();
   const hours = date.getHours();
   const result = timeOfDayArr[Math.trunc(hours / 6) - 1];
@@ -33,6 +33,46 @@ function showGreeting() {
   const timeOfDay = getTimeOfDay();
   greetingText.textContent = `Good ${timeOfDay}`;
 }
+
+//-----------------update bg image
+const body = document.body;
+let randomNum = 1;
+
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setBg() {
+  const timeOfDay = getTimeOfDay();
+  const bgNum = randomNum.toString();
+  const img = new Image();
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum.padStart(2, '0')}.jpg`;
+  img.onload = () => {
+    body.style.backgroundImage = `url("${img.src}"`;
+  };
+}
+
+getRandomNum(1, 20);
+setBg();
+
+//----------------slide bg
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
+function getSlideNext() {
+  randomNum < 20 ? randomNum++ : randomNum = 1;
+  setBg();
+}
+
+function getSlidePrev() {
+  randomNum > 1 ? randomNum-- : randomNum = 20;
+  setBg();
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
 
 // ----------------setTimeout
 function updateMainContext() {
