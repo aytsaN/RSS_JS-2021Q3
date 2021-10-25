22d6d6f40a44ec231311b0c42182402b46f2f501
Ana Shamal <aytsan.shamal@gmail.com>
2021-10-25 21:58:11 +0300

    feat: add settings translation

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -19,6 +19,57 @@ const localization = {
       },
       'error' : 'ERROR: the location not found!',
       'placeholder' : 'Enter City'
+    },
+    'settings' : {
+      'generalHeader' : 'General Settings',
+      'description' : 'Customize your dashboard',
+      'apps' : {
+        'title' : 'Show',
+        'data' : [
+          {
+            'name' : 'Time',
+            'value' : 'time'
+          },
+          {
+            'name' : 'Date',
+            'value' : 'date'
+          },
+          {
+            'name' : 'Greeting',
+            'value' : 'greeting'
+          },
+          {
+            'name' : 'Quote',
+            'value' : 'quote'
+          },
+          {
+            'name' : 'Weather',
+            'value' : 'weather'
+          },
+          {
+            'name' : 'Player',
+            'value' : 'player'
+          }
+        ]
+      },
+      'appearance' : {
+        'title' : 'Appearance',
+        'data' : [{
+          'name' : 'Language',
+          'value' : {
+            'en' : 'Englis',
+            'ru' : 'Russian'
+          }
+        },
+        {
+          'name' : 'Background source',
+          'value' : {
+            'github' : 'GitHub',
+            'unsplash' : 'Unsplash API',
+            'flickr' : 'Flickr API'
+          }
+        }]
+      }
     }
   },
   'ru' : {
@@ -40,6 +91,57 @@ const localization = {
       },
       'error' : 'ОШИБКА: локация не найдена!',
       'placeholder' : 'Введите город'
+    },
+    'settings' : {
+      'generalHeader' : 'Общие настройки',
+      'description' : 'Настройте свою панель управления',
+      'apps' : {
+        'title' : 'Показывать',
+        'data' : [
+          {
+            'name' : 'Время',
+            'value' : 'time'
+          },
+          {
+            'name' : 'Дата',
+            'value' : 'date'
+          },
+          {
+            'name' : 'Приветствие',
+            'value' : 'greeting'
+          },
+          {
+            'name' : 'Цитата',
+            'value' : 'quote'
+          },
+          {
+            'name' : 'Погода',
+            'value' : 'weather'
+          },
+          {
+            'name' : 'Плеер',
+            'value' : 'player'
+          }
+        ]
+      },
+      'appearance' : {
+        'title' : 'Вид',
+        'data' : [{
+          'name' : 'Язык',
+          'value' : {
+            'en' : 'Английский',
+            'ru' : 'Русский'
+          }
+        },
+        {
+          'name' : 'Источник фона',
+          'value' : {
+            'github' : 'GitHub',
+            'unsplash' : 'Unsplash API',
+            'flickr' : 'Flickr API'
+          }
+        }]
+      }
     }
   }
 }
@@ -48,12 +150,14 @@ function setLoacalization(lang) {
   userLanguage = lang;
 }
 
-function updateLocaization() {
-  city.value = localization[userLanguage]['defaultCity'];
+function updateLocaization(oldLang) {
+  if (city.value === localization[oldLang]['defaultCity'])
+    city.value = localization[userLanguage]['defaultCity'];
   getWeather();
   updateMainContext();
   if (!greetingName.value) setNamePlaceholder();
   getQuotes();
+  updateSettingsLang();
 }
 
 //-----------------num randomizer
@@ -137,7 +241,8 @@ function setNamePlaceholder() {
 }
 
 greetingName.addEventListener('change', function() {
-  if (!this.value) {
+  if (!this.value.trim()) {
+    this.value = '';
     setNamePlaceholder();
   }
 });
@@ -250,6 +355,13 @@ function updateMainContext() {
 
 //----------------settings
 
+const settingsBtn = document.querySelector('.settings-icon');
+const settingsContainer = document.querySelector('.settings-app');
+
+const appsListSettingsContainer = document.querySelector('.apps-list');
+const appsCheckboxArr = document.querySelectorAll(('.apps-list input[type="checkbox"]'));
+const optionsArr = document.querySelectorAll(('.toggle-options'));
+
 function isEmptyObject(obj) {
   for (var property in obj) {
       if (obj.hasOwnProperty(property)) return false;
@@ -268,7 +380,6 @@ function toggleSlider(appName) {
     checkbox.checked = false;
     slider.classList.remove('on');
   }
-  console.log(slider);
 }
 
 function toggleApps(activeApps) {
@@ -281,9 +392,29 @@ function toggleApps(activeApps) {
 function toggleOption(optionValue) {
   const oldOption = document.querySelector(`[data-option="${optionValue}"]`).parentNode.querySelector('.active');
   const newOption = document.querySelector(`[data-option="${optionValue}"]`);
-  console.log(oldOption);
   if (oldOption) oldOption.classList.remove('active');
   newOption.classList.add('active');
+}
+
+function updateSettingsLang() {
+  const langSettingsObj = localization[userLanguage]['settings'];
+
+  const generalHeader = settingsContainer.querySelector('.general-header');
+  generalHeader.textContent = langSettingsObj['generalHeader'];
+
+  const settingsDescr = settingsContainer.querySelector('.description');
+  settingsDescr.textContent = langSettingsObj['description'];
+
+  appsListSettingsContainer.previousElementSibling.textContent = langSettingsObj['apps']['title'];
+  document.querySelector('.customize-list').previousElementSibling.textContent = langSettingsObj['appearance']['title'];
+
+  appsCheckboxArr.forEach((el, i) => {
+    el.nextElementSibling.textContent = langSettingsObj['apps']['data'][i]['name'];
+  });
+
+  optionsArr.forEach((el, i) => {
+    el.previousElementSibling.textContent = langSettingsObj['appearance']['data'][i]['name'];
+  });
 }
 
 function getUserSettings() {
@@ -303,13 +434,6 @@ function getUserSettings() {
 
   setLoacalization(state.language);
 }
-
-const settingsBtn = document.querySelector('.settings-icon');
-const settingsContainer = document.querySelector('.settings-app');
-
-const appsListSettingsContainer = document.querySelector('.apps-list');
-const appsCheckboxArr = document.querySelectorAll(('.apps-list input[type="checkbox"]'));
-const optionsArr = document.querySelectorAll(('.toggle-options'));
 
 function toggleSettings() {
   settingsBtn.classList.toggle('settings-open');
@@ -356,15 +480,16 @@ body.addEventListener('click', (e) => {
 
 optionsArr.forEach(options => {
   options.addEventListener('click', (e) => {
-  const optionEl = e.target.closest('.toggle-option');
-  if (!optionEl) return;
-  if (optionEl.classList.contains('active')) return;
-  toggleOption(optionEl.getAttribute('data-option'));
+    const optionEl = e.target.closest('.toggle-option');
+    if (!optionEl) return;
+    if (optionEl.classList.contains('active')) return;
 
     if (optionEl.closest('.custom-lang-toggle')) {
       setLoacalization(optionEl.getAttribute('data-option'));
-      updateLocaization();
+      updateLocaization(optionEl.parentNode.querySelector('.active').getAttribute('data-option'));
     }
+
+    toggleOption(optionEl.getAttribute('data-option'));
   })
 })
 
@@ -381,6 +506,7 @@ function init() {
   getWeather();
   updateMainContext();
   getQuotes();
+  updateSettingsLang();
 }
 
 window.addEventListener('load', init);
